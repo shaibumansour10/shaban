7791560d4e095113360cc3156474aa3c3b4f5d50
shaibu <shaibumansour10@gmail.com>
2024-11-04 22:57:41 -0800

    leo

--- app/(back-office)/dashboard/(catalogue)/categories/new/page.jsx
+++ app/(back-office)/dashboard/(catalogue)/categories/new/page.jsx
@@ -1,13 +1,45 @@
+"use client"
+import TextInput from "@/components/formInputs/TextInput"
 import React from 'react'
 import FormHeader from "@/components/backoffice/FormHeader"
+import { useForm } from "react-hook-form"
 export default function NewCategory() {
+  const {register,handleSubmit,formState:{errors}}=useForm();
+   async function onSubmit(data) {
+    {/*
+            id =>auto
+            title
+            slag=>auto
+            description
+            image
+            
+            */}
+    const slug = generateslug(data.title);
+    data.slug = slug;
+    data.imageUrl = imageUrl;
+    console.log(data);
+    makePostRequest(setLoading, "api/categories", data,
+        "Category", reset
+    );
+    setImageUrl("");
+}
   return (
     <div>
     <FormHeader title="New Category"/>  
-    <form className="w-full max-w-4xl p-4 bg-white border
+    <form 
+    onSubmit={handleSubmit(onSubmit)}
+    className="w-full max-w-4xl p-4 bg-white border
     border-gray-200 rounded-lg shadow sm:p-6 md:p-8
-    dark:bg-gray-800 dark:border-gray-700 mx-auto my-3"></form>
-   
+    dark:bg-gray-800 dark:border-gray-700 mx-auto my-3">
+      <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+    <TextInput label="category Title"
+         name="title"
+         register={register}
+         error={errors}
+         className="w-full"/>
+    </div>
+    </form>
+    
      {/*
      -id
      -title
--- components/FormInputs/TextInput.jsx
+++ components/FormInputs/TextInput.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function TextInput({label,
     name,
     register,
-    errors,
+    errors={},
     isRequired=true,
     type ="text",
     className="sm:col-span-2",
@@ -18,18 +18,18 @@ export default function TextInput({label,
             </label>
             <div className="mt-2">
                 <input
-                {...isRegularExpressionLiteral('${name}',{required: isRequired})}
+                {...register(`${name}`,{required: isRequired})}
                 type={type}
                 name={name}
                 id={name}
                 defaultValue={defaultValue}
                 autoComplete={name}
                 className="block w-full rounded-md border-0 py-3 text-gray-900
-                 shadow-sm ring-1"
-                placeholder={'Type the ${label.toLOwerCase()}'} 
+                 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lime-600 sm:text-sm"
+                placeholder={`Type the ${label.toLowerCase()}`} 
                 
                 />
-                {errors['$(name)']&& (
+                {errors[`${name}`] && (
                     <span className="text-sm text-red-600">{lebel}is required</span>
                 )}
             </div>
